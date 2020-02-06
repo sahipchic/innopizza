98c1fb57936cb9c775c4a918d67ecf7c861bf0aa
sahipchic <ilja.sahipoff@yandex.ru>
2020-02-06 13:26:55 +0300

    fix height of pizza card

--- index.php
+++ index.php
@@ -263,7 +263,7 @@ function getActiveReviews() {
                     $tmp_out .= "<div class=\"row\">";
                 }
                 $tmp_out .= "<div class=\"col-xs-12 col-sm-12 col-md-6 col-lg-4\">
-                        <div class=\"card m-4\" style='height: 500px;'>
+                        <div class=\"card m-4\" style='height: 550px;'>
                         <img class=\"card-img-top mb-3\" src=\"img/$img_link\" alt=\"Card image\">
                         <div class=\"card-block\">
                           <h4 class=\"card-title\">$name</h4>
