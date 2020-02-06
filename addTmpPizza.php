98054f0b2ce3b32a91c2770663d818929c6dfde7
sahipchic <ilja.sahipoff@yandex.ru>
2020-02-06 03:50:19 +0300

    try to find bug

--- addTmpPizza.php
+++ addTmpPizza.php
@@ -3,7 +3,7 @@ session_start();
 if (isset($_SESSION['pizzas'])) {
     $data = json_decode(file_get_contents('php://input'), true);
     if (isset($data['pizzaId'])) {
-        array_push($_SESSION['pizzas'], $data['pizzaId']);
+        $_SESSION['pizzas'][] = $data['pizzaId'];
     }
     echo 'The product was successfully added to the cart';
 }
